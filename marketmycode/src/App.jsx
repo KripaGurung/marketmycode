aea5e6b500906a81feb7744acbb1661a2e2f206c
KripaGurung <krepagurung@gmail.com>
2025-12-02 16:44:57 +0545

    toast was added

--- marketmycode/src/App.jsx
+++ marketmycode/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Signup from './pages/signup/signup.jsx';
 import Login from './pages/login/login.jsx'
 
@@ -7,6 +9,9 @@ function App() {
   
   return (
     <>
+
+     <ToastContainer position="top-center" autoClose={2000} />
+     
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
