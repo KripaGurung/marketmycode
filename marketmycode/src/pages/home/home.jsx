be8d60399e0047349af47357967581f0b5213377
KripaGurung <krepagurung@gmail.com>
2025-12-07 15:39:36 +0545

    featured section was added

--- marketmycode/src/pages/home/home.jsx
+++ marketmycode/src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../../component/Header/Header.jsx";
+import FeaturedProjects from '../../component/Featured Projects/FeaturedProjects.jsx';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <FeaturedProjects />
         </div>
     )
 }
